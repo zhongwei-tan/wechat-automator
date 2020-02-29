ed012d025a884f218c115146dda3c0a8dabad75c
zhongweitan <zhongwei.tan@hotmail.com>
2020-02-29 10:57:25 +0100

    feat(automator): Added confirmed chatroom search

--- src/wechat.py
+++ src/wechat.py
@@ -1,5 +1,6 @@
 import itchat
-
+import time
+import yaml
 
 def login_callback():
     print('Login successful')
@@ -13,15 +14,35 @@ def wechat_login():
     itchat.auto_login(hotReload=True, statusStorageDir='itchat.pkl', enableCmdQR=2,
                       loginCallback=login_callback, exitCallback=logout_callback)
 
+def search_chatrooms(chatroom_name: str):
+    found_chatroom = None
+    start_time = time.time()
+    while not found_chatroom and time.time() - start_time < 10:
+        found_chatroom = itchat.search_chatrooms(name=chatroom_name)
+    if found_chatroom:
+        print(f"Chatroom \"{chatroom_name}\" found.")
+        return found_chatroom[0]
+    else:
+        print(f"Chatroom \"{chatroom_name}\" not found!")
+        return None
+
 def send_message_to_chatroom(message: str, chatroom_name: str):
     try:
-        chatroom = itchat.search_chatrooms(name=chatroom_name)[0]
+        chatroom = search_chatrooms(chatroom_name)
         chatroom.send(message)
-        print('Message sent!')
+        print("Message sent.")
     except IndexError:
-        print('Chatroom {} is not found'.format(chatroom_name))
+        print("Message not sent!")
 
 
 # Login wechat at startup
+# Check if all chatrooms in reminder_list.yml are found
 if __name__ == "__main__":
     wechat_login()
+    
+    print("Checking chatrooms...")
+    with open("reminder_list.yml", "r") as file:
+        reminders = yaml.load(file, Loader=yaml.FullLoader)
+        chatrooms = set(reminder["chatroom"] for reminder in reminders.values())
+    for chatroom in chatrooms:
+        search_chatrooms(chatroom)
