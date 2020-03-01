592a696d707302af97794ccf856331ec0a563b23
zhongweitan <zhongwei.tan@hotmail.com>
2020-03-01 10:11:06 +0100

    fix(wechat): switch back to print

--- src/wechat.py
+++ src/wechat.py
@@ -1,15 +1,13 @@
 import itchat
 import time
 import yaml
-from celery.utils.log import get_task_logger
 
-logger = get_task_logger(__name__)
 
 def login_callback():
-    logger.info("Login successful")
+    print("Login successful")
 
 def logout_callback():
-    logger.info("Logout")
+    print("Logout")
     itchat.auto_login(hotReload=True, statusStorageDir="itchat.pkl", enableCmdQR=2,
                       loginCallback=login_callback, exitCallback=logout_callback)
 
@@ -23,19 +21,19 @@ def search_chatrooms(chatroom_name: str):
     while not found_chatroom and time.time() - start_time < 10:
         found_chatroom = itchat.search_chatrooms(name=chatroom_name)
     if found_chatroom:
-        logger.info(f"Chatroom \"{chatroom_name}\" found.")
+        print(f"Chatroom \"{chatroom_name}\" found.")
         return found_chatroom[0]
     else:
-        logger.info(f"Chatroom \"{chatroom_name}\" not found!")
+        print(f"Chatroom \"{chatroom_name}\" not found!")
         return None
 
 def send_message_to_chatroom(message: str, chatroom_name: str):
     try:
         chatroom = search_chatrooms(chatroom_name)
         chatroom.send(message)
-        logger.info("Message sent.")
+        print("Message sent.")
     except IndexError:
-        logger.info("Message not sent!")
+        print("Message not sent!")
 
 
 # Login wechat at startup
@@ -44,7 +42,7 @@ def send_message_to_chatroom(message: str, chatroom_name: str):
 if __name__ == "__main__":
     wechat_login()
 
-    logger.info("Checking chatrooms...")
+    print("Checking chatrooms...")
     with open("reminder_list.yml", "r") as file:
         reminders = yaml.load(file, Loader=yaml.FullLoader)
         chatrooms = set(reminder["chatroom"] for reminder in reminders.values())
